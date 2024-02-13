f21c31c6d613d2d03790e40d80e6acef11ad6d04
Ramandeep Ramarayane <ramandeep.r49@gmail.com>
2024-02-14 01:57:30 +0530

    config update

--- src/config/config.js
+++ src/config/config.js
@@ -3,4 +3,4 @@ export const TEST_PORT = 3000;
 
 const TEST_URL = `https://nonprod-api.bulkindexer.net`;
 
-export const BACKEND_URL = PROD ? `http://localhost:${TEST_PORT}` : TEST_URL;
+export const BACKEND_URL = PROD ? TEST_URL : TEST_URL;
